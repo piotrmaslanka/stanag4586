bde121c07191d424325bf2d164cc6e196523abe2
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-04-30 23:34:23 +0200

    fix build

--- compile_stanag.py
+++ compile_stanag.py
@@ -95,7 +95,7 @@ def process_a_module(depo_name: str, module_name: str, module_desc: dict):
 
     docs_args = [f'{depo_name}\n',
                  '=' * len(depo_name) + '\n',
-                 f'.. autoclass:: stanag4586.{depo_name}.{class_name}\n'.
+                 f'.. autoclass:: stanag4586.{depo_name}.{class_name}\n',
                  f'    :members:\n']
 
     doc_file.write(''.join(docs_args))
